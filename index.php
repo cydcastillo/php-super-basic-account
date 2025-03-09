95651c0d9f5f77e9674a4b8f50faa2e09519a132
ycydro <cydicastillo@gmail.com>
2025-03-09 22:46:42 +0800

    feat: log in

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Login</title>
+</head>
+
+<body>
+    <div class="container">
+        <form action= "loggedin.php" method = "POST">
+            <label for="un">Username: </label>
+            <input type="text" name= "un">
+            <label for="pass">Password: </label>
+            <input type="text" name= "pass">
+            <button type="submit">SUBMIT</button>
+        </form>
+    </div>
+</body>
+</html>
---
+++ loggedin.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logged In!</title>
+</head>
+    <?php
+            function compareCredentials ($un, $pass) {
+                $username = "admin";
+                $password = "admin123";
+
+                if ($un == $username) {
+                    if ($pass == $password) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $un = $_POST["un"];
+                $pass = $_POST["pass"];
+
+                if(compareCredentials($un, $pass)) {
+                    echo "Logged in";
+                } else {
+                    echo "Wrong Credentials!";
+                }
+        }
+    ?>
+<body>
+    
+</body>
+</html>
